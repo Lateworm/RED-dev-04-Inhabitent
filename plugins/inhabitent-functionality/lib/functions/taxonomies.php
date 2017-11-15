2b982f2d26a9d31274f6ac14ef7e5ae50e473cbd
Late Worm <travis.rempel@gmail.com>
2017-11-14 23:13:31 -0800

    Add Products post type and taxonomy

--- plugins/inhabitent-functionality/lib/functions/taxonomies.php
+++ plugins/inhabitent-functionality/lib/functions/taxonomies.php
@@ -6,3 +6,42 @@
  */
 
 // Add your custom taxonomies here...
+
+// Register Custom Taxonomy
+function inhabitent_taxonomy_product_type() {
+
+	$labels = array(
+		'name'                       => 'Product Types',
+		'singular_name'              => 'Product Type',
+		'menu_name'                  => 'Product Types',
+		'all_items'                  => 'All Product Types',
+		'parent_item'                => 'Parent Product Type',
+		'parent_item_colon'          => 'Parent Product Type:',
+		'new_item_name'              => 'New Product Type Name',
+		'add_new_item'               => 'Add New Product Type',
+		'edit_item'                  => 'Edit Product Type',
+		'update_item'                => 'Update Product Type',
+		'view_item'                  => 'View Product Type',
+		'separate_items_with_commas' => 'Separate product types with commas',
+		'add_or_remove_items'        => 'Add or remove product types',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Product typess',
+		'search_items'               => 'Search Items',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No items',
+		'items_list'                 => 'Items list',
+		'items_list_navigation'      => 'Items list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'product-type', array( 'product' ), $args );
+
+}
+add_action( 'init', 'inhabitent_taxonomy_product_type', 0 );
