cec4623218270df653500061960975fcb0a528c5
Late Worm <travis.rempel@gmail.com>
2017-11-17 20:17:35 -0800

    Register Adventures Post Type

--- plugins/inhabitent-functionality/lib/functions/post-types.php
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -7,7 +7,7 @@
 
 // Add your custom post types here...
 
-// Register Custom Post Type
+// Register Products Post Type
 function inhabitent_cpt_product() {
 
 	$labels = array(
@@ -17,7 +17,7 @@ function inhabitent_cpt_product() {
 		'name_admin_bar'        => 'Product',
 		'archives'              => 'Product Archives',
 		'attributes'            => 'Product Attributes',
-		'parent_item_colon'     => 'Parent Product',
+		'parent_item_colon'     => 'Parent Product:',
 		'all_items'             => 'All Products',
 		'add_new_item'          => 'Add New Product',
 		'add_new'               => 'Add New',
@@ -63,3 +63,63 @@ function inhabitent_cpt_product() {
 
 }
 add_action( 'init', 'inhabitent_cpt_product', 0 );
+
+
+
+
+// Register Adventures Post Type
+function inhabitent_cpt_adventure() {
+
+	$labels = array(
+		'name'                  => 'Adventures',
+		'singular_name'         => 'Adventure',
+		'menu_name'             => 'Adventures',
+		'name_admin_bar'        => 'Adventure',
+		'archives'              => 'Adventure Archives',
+		'attributes'            => 'Adventure Attributes',
+		'parent_item_colon'     => 'Parent Product:',
+		'all_items'             => 'All Adventures',
+		'add_new_item'          => 'Add New Adventure',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Adventure',
+		'edit_item'             => 'Edit Adventure',
+		'update_item'           => 'Update Adventure',
+		'view_item'             => 'View Adventure',
+		'view_items'            => 'View Adventures',
+		'search_items'          => 'Search Adventures',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Adventure',
+		'uploaded_to_this_item' => 'Uploaded to this adventure',
+		'items_list'            => 'Adventures list',
+		'items_list_navigation' => 'Adventures list navigation',
+		'filter_items_list'     => 'Filter Adventures list',
+	);
+	$args = array(
+		'label'                 => 'Adventure',
+		'description'           => 'an adventure post type',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-location-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'adventure', $args );
+
+}
+add_action( 'init', 'inhabitent_cpt_adventure', 0 );
