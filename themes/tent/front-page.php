691676f2daf825c73e8ee3008afc906b6e7587d8
Late Worm <travis.rempel@gmail.com>
2017-11-19 16:15:05 -0800

    Add Adventures section

--- themes/tent/front-page.php
+++ themes/tent/front-page.php
@@ -14,7 +14,7 @@ get_header(); ?>
 	<img class="entry-title" src="<?php echo get_stylesheet_directory_uri()?>/img/inhabitent-logo-full.svg" >
 </div>
 
-<div id="content" class="site-content-wide">
+<div id="content" class="site-content-wide front-page-php">
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -39,7 +39,6 @@ get_header(); ?>
 
 		<h2>Shop Stuff</h2>
 		<section class="product-info container">
-
 			<?php /* start a secondary loop to return product posts */
 					$terms = get_terms( array(
 							'taxonomy' => 'product-type',
@@ -67,7 +66,6 @@ get_header(); ?>
 
 		<h2>Inhabitent Journal</h2>
 		<section class="front-page-journal">
-			
 			<?php /* start a secondary loop to return posts */
 				$args = array( 'post_type' => 'post', 'order' => 'DESC', 'posts_per_page' => 3, );
 				$journal_posts = get_posts( $args ); // returns an array of posts
@@ -76,13 +74,36 @@ get_header(); ?>
 
 				<div class="front-page-journal-entry">
 					<?php the_post_thumbnail( 'medium' ); ?>
-					<span><?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></span>
-					<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+
+					<div class="front-page-journal-entry-text">
+						<span><?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></span>
+						<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+					</div>
+
 				</div>
 
 			<?php endforeach; wp_reset_postdata(); ?>
 		</section>
 
+
+		<h2>Latest Adventures</h2>
+		<section class="front-page-adventures">
+			<?php /* start a secondary loop to return posts */
+				$args = array( 'post_type' => 'adventure', 'order' => 'ASC', 'posts_per_page' => 4, );
+				$journal_posts = get_posts( $args ); // returns an array of posts
+				foreach ( $journal_posts as $post ) : setup_postdata( $post );
+			?>
+				
+				<div class="front-page-adventure-entry" style="background-image: url()">
+					<div class="front-page-adventure-entry-text">
+						<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+					</div>
+
+				</div>
+
+			<?php endforeach; wp_reset_postdata(); ?>
+		</section>
+		<a href="<?php echo site_url(); ?>/adventure"><h3>More Adventures</h3></a>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
