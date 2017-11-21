a6484c0e52cf036814f1eaab251e5438520b2925
Late Worm <travis.rempel@gmail.com>
2017-11-20 18:29:18 -0800

    Add logo and search to navbar

--- themes/tent/header.php
+++ themes/tent/header.php
@@ -21,16 +21,24 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-
+					<a href="<?php echo site_url(); ?>">
+						<img src="<?php echo get_stylesheet_directory_uri()?>/img/inhabitent-logo-tent.svg" >
+					</a>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+
+			
+					<i  id="search-button" class="fa fa-search" aria-hidden="true"><span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span></i>
+					<?php get_search_form(); ?>
+
 				</nav><!-- #site-navigation -->
+
 			</header><!-- #masthead -->
 
 			
