493389c32b20e326359b1a6ca1b69ce0e2e9c4e0
Late Worm <travis.rempel@gmail.com>
2017-11-22 13:54:20 -0800

    Use code from content.php to complete the layout

--- themes/tent/single.php
+++ themes/tent/single.php
@@ -9,11 +9,30 @@ get_header(); ?>
 <div id="content" class="site-content">
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main site-main-journal" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<!-- Used to call "content-single" template part -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+					<?php if ( 'post' === get_post_type() ) : ?>
+					<div class="entry-meta">
+						<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+					</div><!-- .entry-meta -->
+					<?php endif; ?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
 
 			<?php the_post_navigation(); ?>
 
